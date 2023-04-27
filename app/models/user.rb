7f1f4b49257fb74b4f7e68185b28eb570f7262cb
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-27 13:10:37 -0500

    merging

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :rooms, dependent: :destroy
+  has_many :reservations
+  has_many :reviews, through: :reservations
 end
+
+#dependent: :destroy specifies that when a record in the model is destroyed, any associated records should also be destroyed. For example, when a User is destroyed, all of their associated rooms, reservations, and reviews should also be destroyed.
