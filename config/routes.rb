d16420e52ab6ae21c7fff0be77aa0ffb1f021e98
Tom Gagliano <tgaglianoll@gmail.com>
2023-04-27 14:42:13 -0400

    add routes for rooms, reservations, reviews, room_images, and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,37 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+get "/rooms" => "rooms#index"
+post "/rooms" => "rooms#new"
+get "/rooms/:id" => "rooms#show"
+post "/rooms" => "room#create"
+patch "/rooms/:id" => "rooms#update"
+delete "/rooms/:id" => "rooms#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+get "/reservations" => "reservations#index"
+post "/reservations" => "reservations#new"
+get "/reservations/:id" => "reservations#show"
+post "/reservations" => "reservations#create"
+patch "/reservations/:id" => "reservations#update"
+delete "/reservations/:id" => "reservations#destroy"
+
+get "/reviews" => "reviews#index"
+post "/reviews" => "reviews#new"
+get "/reviews/:id" => "reviews#show"
+post "/reviews" => "reviews#create"
+patch "/reviews/:id" => "reviews#update"
+delete "/reviews/:id" => "reviews#destroy"
+
+get "/room_images" => "room_images#index"
+post "/room_images" => "room_images#new"
+get "/room_images/:id" => "room_images#show"
+post "/room_images" => "room_images#create"
+patch "/room_images/:id" => "room_images#update"
+delete "/room_images/:id" => "room_images#destroy"
+
+get "/users" => "users#index"
+post "/users" => "users#new"
+get "/users/:id" => "users#show"
+post "/users" => "users#create"
+patch "/users/:id" => "users#update"
+delete "/users/:id" => "users#destroy"
+
 end
