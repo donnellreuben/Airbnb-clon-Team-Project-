d34555e17e6adf3f4741bfba2a1dc5d959623923
Tom Gagliano <tgaglianoll@gmail.com>
2023-04-28 16:22:59 -0400

    add review routes and new.html.erb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,20 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
     render :index
   end
+
+  def new
+    @review = Review.new
+    render template: "reviews/new"
+  end
+
+# CREATE
+  def create
+    @review = Review.new(
+      reservation_id: params[:review][:reservation_id],
+      rating: params[:review][:rating],
+      comment: params[:review][:comment]
+     )
+    @review.save!
+    render json: {message: "Thank you for your review!"}
+  end 
 end
